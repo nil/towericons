a48437a30eea690f7e423812fbd1b204bd969bf0
Nil Vila <nilvilamar@gmail.com>
2020-03-13 17:02:57 +0100

    Activated script to rename export folder

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ const shell = require('shelljs');
 
 // shell.echo('')
 
-// // Creates icons/ directory if it doesn't exist
-// if (!shell.test('-e', 'icons/')) {
-//   shell.mkdir('icons')
-// }
+// Creates icons/ directory if it doesn't exist
+if (!shell.test('-e', 'icons/')) {
+  shell.mkdir('icons')
+}
 
-// // Renames all files inside icon/ directory
-// shell.find('icon /*').forEach(function (file) {
-//   if (file.includes('.')) {
-//     const slashExp = new RegExp(' ?\/ ?', 'gi');
-//     const iconsExp = new RegExp('icon-', 'gi');
-//     const miscExp = new RegExp('misc-', 'gi');
-//     const newName = file.replace(slashExp, '-').replace(iconsExp, 'icons/').replace(miscExp, '');
+// Renames all files inside icon/ directory
+shell.find('icon /*').forEach(function (file) {
+  if (file.includes('.')) {
+    const slashExp = new RegExp(' ?\/ ?', 'gi');
+    const iconsExp = new RegExp('icon-', 'gi');
+    const miscExp = new RegExp('misc-', 'gi');
+    const newName = file.replace(slashExp, '-').replace(iconsExp, 'icons/').replace(miscExp, '');
 
-//     shell.mv(file, newName);
-//   }
-// })
+    shell.mv(file, newName);
+  }
+})
 
 // Remove icon/ directory
 shell.rm('-r', 'icon /')
