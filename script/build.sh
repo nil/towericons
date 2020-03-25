43c2218ec3325a7936e9f974236c059c7a4adf36
Nil Vila <nilvilamar@gmail.com>
2020-03-25 15:07:05 +0100

    Add build bash script

---
+++ script/build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Start text
+echo "Start building Nilicon icons and components"
+
+rm -rf icons
+echo "✅ icons directory removed"
+
+unzip -o -d icons icons.zip
+rm icons.zip
+echo "✅ Unzip completed"
+
+npm run svgo
+echo "✅ Icons optimized"
+
+mkdir 'temp-icons'
+echo "✅ temp-icons direcotry created"
+
+node script/rename.js
+echo "✅ Icons copied to a new directory"
+
+npm run svgr
+echo "✅ React component files created"
+
+npm run create
+# Success messages are inside the script
+
+rm -rf 'temp-icons'
+echo "✅ temp-icons directory removed"
+
+echo ""
+echo "✨  Success"
